de89e07dc5e5db673f01d6be3b7456dd2f833241
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-15 22:37:48 -0300

    feat: finalizando desafio de recursao

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: calculate_length(list, 0)
+
+  defp calculate_length([], acc), do: acc
+  defp calculate_length([_head | tail], acc) do
+    acc = acc + 1
+
+    calculate_length(tail, acc)
   end
 end
